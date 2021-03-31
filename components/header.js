bf6d954100e0b8a47aba85c4486d77ad62590964
Michał Scisłowski <m.scislowski@protonmail.com>
2021-03-31 14:10:57 +0200

    nie wazne

--- components/header.js
+++ components/header.js
@@ -5,22 +5,13 @@ export default function Header() {
   return (
     <div className="main">
       <header className="header">
-        <ul>
-          <li className="liLogo">
-            <a className="logo" href="#">KUPBSV</a>
-          </li>
-          <li>
-            <a className="item" href="#">Money Button</a>
-          </li>
-          <li>
-            <a className="item" href="#">Login</a>
-          </li>
-          <li>
-            <a className="item"><SimpleMenu /></a>
-          </li>
-        </ul>
+        <a className="logo" href="#">KUPBSV</a>
+        <a className="push" href="#">Money Button</a>
+        <a className="item" href="#">Login</a>
+        <a className="item"><SimpleMenu /></a>
       </header>
 
+<<<<<<< HEAD
       <style jsx> {`
         .main {
           position: fixed;
@@ -79,6 +70,49 @@ export default function Header() {
 
       
       `} </style>
+=======
+      <style jsx>{`
+  .main {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: auto;
+    background-color: black;
+    font-size: 25px;	
+    font-weight: 300;	
+  }
+  a {
+    color: white;
+    letter-spacing: 2px;
+    text-decoration: none;
+    padding: 20px 15px;
+    justify-content: flex-end;
+  }
+   .logo {
+    font-weight: 700;
+    margin-left: 15px;
+    font-size: 25px;
+    float: left;
+   }
+   
+   .push {
+    margin-left: auto;
+  }
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  @media only screen and (max-width: 555px) {
+    .item, .push {
+      font-size: 18px;	
+      margin auto;
+    }
+  }
+
+`}</style>
+>>>>>>> 31fcf50385ca0e9eab5f159222aea264fea230e5
     </div>
   );
 }
