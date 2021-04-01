a75d73340bdaed8f3668e7b707b49d2a291c4bb7
Michał Scisłowski <m.scislowski@protonmail.com>
2021-04-01 22:01:05 +0200

    prevent non numeric values in calculatorsell.js

--- components/calculatorsell.js
+++ components/calculatorsell.js
@@ -20,6 +20,11 @@ export default function Calculatorsell() {
                     InputProps={{ inputProps: { min: "0.01", max: "5000", step: "0.0001" } }}
                     variant="outlined"
                     onChange={(e) => setValue(e.currentTarget.value)}
+                    onKeyPress={(e) => {
+                        if (!/[0-9]/.test(e.key)) {
+                          e.preventDefault();
+                        }
+                      }}
                     onBlur={(e) => {
                       if (e.currentTarget.value > 0.0001 & e.currentTarget.value < 0.01 ) setValue(0.01);
                     }}
