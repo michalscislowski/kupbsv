549034922ae99efe982116de9126f9f6bf7f2334
Michał Scisłowski <m.scislowski@protonmail.com>
2021-04-13 16:02:31 +0200

    MoneyButton 0Auth handler v0.0.1

--- components/auth.js
+++ components/auth.js
@@ -4,5 +4,5 @@ const { MoneyButtonClient } = require('@moneybutton/api-client')
 export default async function Authuser () {
     const client = new MoneyButtonClient('1c098ac11f57bef8e51899f75c03c34e')
     client.requestAuthorization('auth.user_identity:read','http://localhost:3000')
-    client.handleAuthorizationResponse()
+    setTimeout(client.handleAuthorizationResponse(), 100);
 }
