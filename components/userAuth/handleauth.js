9d7c086d306357a4667bc353a0bb0fd4faad29e3
j3ksa <yogalj@DESKTOP-CLB1F7G.localdomain>
2021-05-18 16:09:57 +0200

    added one more redirect to /

--- components/userAuth/handleauth.js
+++ components/userAuth/handleauth.js
@@ -2,8 +2,8 @@ const { MoneyButtonClient } = require('@moneybutton/api-client')
 import {useRouter} from "next/router";
 
 export default async function handleAuthuser () {
-    //const client = new MoneyButtonClient('1c098ac11f57bef8e51899f75c03c34e');
-    const client = new MoneyButtonClient('cd8072b2a8b1557cc7ad71d96d038658');
+    const client = new MoneyButtonClient('1c098ac11f57bef8e51899f75c03c34e');
+    // const client = new MoneyButtonClient('cd8072b2a8b1557cc7ad71d96d038658');
 	const router = useRouter()
     
     client.handleAuthorizationResponse().then(router.push('/home', undefined));
