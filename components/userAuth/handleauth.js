a8c1652b38543e77199d699319a9c692a55f415a
leisik <37248066+leisik@users.noreply.github.com>
2021-05-18 17:58:41 +0200

    Added new page to semi fix problem with loading front page. Now after a user is comming back from money button he is redirected to home page which is copy of index js

--- components/userAuth/handleauth.js
+++ components/userAuth/handleauth.js
@@ -1,12 +1,17 @@
 const { MoneyButtonClient } = require('@moneybutton/api-client')
-
+import {useRouter} from "next/router";
 //onst { MoneyButtonClient } = require('@moneybutton/api-client')
 export default async function handleAuthuser () {
-    //const client = new MoneyButtonClient('1c098ac11f57bef8e51899f75c03c34e');
+    const router = useRouter()
+    // const client = new MoneyButtonClient('1c098ac11f57bef8e51899f75c03c34e');
     const client = new MoneyButtonClient('cd8072b2a8b1557cc7ad71d96d038658');
     
-    client.handleAuthorizationResponse();
-
+    client.handleAuthorizationResponse().then(router.push('/home', undefined));
+    // setTimeout(() => {
+        
+    // }, 1000);
+    
+    
     //const refreshToken = client.getRefreshToken();
 
 }
