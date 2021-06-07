0a8726001c0f67be1b2f39c456e831bb1cec2af1
leisik <37248066+leisik@users.noreply.github.com>
2021-06-07 02:24:56 +0200

    Added max balance button to calculatorSell

--- components/calculatorsell.js
+++ components/calculatorsell.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import TextField from '@material-ui/core/TextField';
+import {useRecoilState} from 'recoil'
+import {recoilUserAmount} from './states'
+import Button from '@material-ui/core/Button'
 
 
 export default function Calculatorsell(props) {
     const [value, setValue] = useState(0);
-
+    const userAmount = useRecoilState(recoilUserAmount);
+    const tmp_userAmount = useState(parseFloat((userAmount[0].satoshis)/100000000));
+    console.log("SELL ", tmp_userAmount[0]);
+    console.log("SELL ", userAmount[0]);
     return (
         <div>
             <div className="typebox">
                 <div className="textfield">
-                    <TextField error={false} id="outlined-number" label="BSV" helperText="min. 0.01 BSV"  
+                    <TextField error={false} id="outlined-number" label="BSV"
                     type="tel"
                     value={value}
                     InputProps={{ inputProps: { min: "0.01", max: "5000", step: "0.01" } }}
@@ -25,6 +31,7 @@ export default function Calculatorsell(props) {
                     }}
                     />
                 </div>
+                <Button style={{position: 'absolute', top: 65, left: 10, padding: 5, height: 25, width: 180, textAlign: 'center', textTransform: 'none', color: '#808080'}} onClick={()=> {setValue(tmp_userAmount[0])}}>{"max " +  tmp_userAmount[0].toFixed(5) + ' BSV'}</Button>
 
                 <div className="textfield">
                     <TextField disabled id="outlined-disabled" value={(value*props.cena).toFixed(2)} label="PLN" variant="outlined" />
@@ -37,6 +44,7 @@ export default function Calculatorsell(props) {
                     height: auto;
                     display: flex;
                     justify-content: center;
+                    position: relative;
                 }
                 .textfield {
                     margin: 5px 10px 10px;
